b72bc92fe91c57ec3b844f589ba8044f9a7837b2
ovalente <ovalente@solutionsmedias360.ca>
2016-08-02 16:26:07 -0400

    Add 10 second time to cronitor call. Don't try to call cronitor if we don't have a monitor code and log warn when it happend

--- src/main/java/ca/sm360/cronitor/client/CronitorClient.java
+++ src/main/java/ca/sm360/cronitor/client/CronitorClient.java
@@ -4,12 +4,16 @@ import ca.sm360.cronitor.client.urlgenerators.CompleteCommandUrlGenerator;
 import ca.sm360.cronitor.client.urlgenerators.FailCommandUrlGenerator;
 import ca.sm360.cronitor.client.urlgenerators.PauseCommandUrlGenerator;
 import ca.sm360.cronitor.client.urlgenerators.RunCommandUrlGenerator;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class CronitorClient {
+
+    private final static Logger logger = Logger.getLogger(CronitorClient.class.getName());
 
     private CronitorPinger cronitorPinger = new CronitorPinger();
 
@@ -26,44 +30,80 @@ public class CronitorClient {
     }
 
     public void run(String monitorCode) throws IOException, URISyntaxException {
-
-        URL commandUrl = new RunCommandUrlGenerator().buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new RunCommandUrlGenerator().buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void run(String monitorCode, String message) throws IOException, URISyntaxException {
-
-        URL commandUrl = new RunCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new RunCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void complete(String monitorCode) throws URISyntaxException, IOException {
-        URL commandUrl = new CompleteCommandUrlGenerator().buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new CompleteCommandUrlGenerator().buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void complete(String monitorCode, String message) throws URISyntaxException, IOException {
-        URL commandUrl = new CompleteCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new CompleteCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void fail(String monitorCode) throws URISyntaxException, IOException {
-        URL commandUrl = new FailCommandUrlGenerator().buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new FailCommandUrlGenerator().buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void fail(String monitorCode, String message) throws URISyntaxException, IOException {
-        URL commandUrl = new FailCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new FailCommandUrlGenerator().withMessage(message).buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't ping cronitor because the monitor code pass was null or empty");
+        }
     }
 
     public void pause(String monitorCode, int timeoutInHouirs) throws URISyntaxException, IOException {
-        URL commandUrl = new PauseCommandUrlGenerator(timeoutInHouirs).buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new PauseCommandUrlGenerator(timeoutInHouirs).buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't pause monitor because the monitor code pass was null or empty");
+        }
     }
 
     public void unpause(String monitorCode) throws URISyntaxException, IOException {
-        URL commandUrl = new PauseCommandUrlGenerator(0).buildURI(monitorCode, apiKey);
-        cronitorPinger.ping(commandUrl);
+
+        if(StringUtils.isNotBlank(monitorCode)) {
+            URL commandUrl = new PauseCommandUrlGenerator(0).buildURI(monitorCode, apiKey);
+            cronitorPinger.ping(commandUrl);
+        } else {
+            logger.warning("we can't pause monitor because the monitor code pass was null or empty");
+        }
     }
 }
--- src/main/java/ca/sm360/cronitor/client/CronitorPinger.java
+++ src/main/java/ca/sm360/cronitor/client/CronitorPinger.java
@@ -2,6 +2,7 @@ package ca.sm360.cronitor.client;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.logging.Logger;
@@ -10,13 +11,22 @@ public class CronitorPinger {
 
     private final static Logger logger = Logger.getLogger(CronitorPinger.class.getName());
 
+    private final Integer cronitorPingTimeoutInSecond = 10;
+
     public void ping(URL commandURL) throws IOException, URISyntaxException {
 
         logger.info("Will call url : " + commandURL);
 
         HttpURLConnection connection = (HttpURLConnection) commandURL.openConnection();
-        connection.connect();
-        connection.getInputStream();
-        connection.disconnect();
+        connection.setConnectTimeout(cronitorPingTimeoutInSecond * 1000);
+
+        try {
+            connection.connect();
+            connection.getInputStream();
+            connection.disconnect();
+        } catch (SocketTimeoutException e) {
+            logger.warning(String.format("fail to call url [%s] because of a timeout trigger after %d seconds",
+                    commandURL, cronitorPingTimeoutInSecond));
+        }
     }
 }
